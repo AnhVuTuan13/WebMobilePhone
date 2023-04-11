35b72a63c3f5cb7b2350eb592257a50b4d2c29d7
AnhVuTuan13 <tuananh130201@gmail.com>
2023-04-11 20:45:11 +0700

    V6.3

---
+++ WebMobilePhone_Website/Controllers/NewsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMobilePhone_DataAccess.Infrastructures;
+using X.PagedList;
+using WebMobilePhone_Models.Models;
+using WebMobilePhone_Website.Models;
+
+namespace WebMobilePhone_Website.Controllers
+{
+    public class NewsController : Controller
+    {
+        public readonly IUnitOfWork unitOfWork;
+
+        public NewsController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index(int? page)
+        {
+            //lay trang hien tai
+            //page ?? 1
+            //neu page khac null thi _CurrentPage = page
+            //neu page =  null thi _CurrentPage = 1
+            int _CurrentPage = page ?? 1;
+            //quy dinh so ban ghi tren mot trang
+            int _RecordPerPage = 20;
+            //---
+            List<News> listRecord = new List<News>();
+            listRecord = unitOfWork.NewsRepository.GetAll().OrderByDescending(anhxa => anhxa.ID).ToList();
+            //---
+            //truyen gia tri ra view co phan trang
+            return View("Index", listRecord.ToPagedList(_CurrentPage, _RecordPerPage));
+        }
+        //chi tiet san pham
+        public IActionResult Detail(int? id)
+        {
+            int intID = id ?? 0;
+            News record = unitOfWork.NewsRepository.Find(intID);
+            return View("Detail", record);
+        }
+    }
+}
